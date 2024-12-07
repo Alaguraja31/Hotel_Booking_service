2d76b4277840c0e2f161e36aa99262933f8133e6
Alaguraja31 <raja2000eswar@gmail.com>
2024-12-07 12:53:21 +0530

    Added server files

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
+// server.js
 const jsonServer = require('json-server')
-
 const server = jsonServer.create()
-
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
-const cors=require('cors');
-server.use(cors())
-
+ 
 server.use(middlewares)
-
-server.use('/api', router)
-
+server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running')
 })
